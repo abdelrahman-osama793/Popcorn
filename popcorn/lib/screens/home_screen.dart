9f406b61b17ed9b3d5fa685fade65e10f1fccda8
Abd El Rahman Osama <aerosama793@gmail.com>
2021-03-13 15:44:23 +0200

    rename search_screens to search_screen

--- popcorn/lib/screens/home_screen.dart
+++ popcorn/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:popcorn/screens/search_screens.dart';
+import 'package:popcorn/screens/search_screen.dart';
 import 'package:popcorn/style/theme.dart' as style;
 import 'package:popcorn/widget/genre_widget.dart';
 import 'package:popcorn/widget/now_playing_widget.dart';
